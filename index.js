3c27738d14033627ab5d78a005b9793577ad5d4b
BarayAlberto <beto450xd@gmail.com>
2022-04-26 13:32:00 -0600

    Index.js con un mensaje de Hola Mundo en HTML y Nueva version en package.json

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const server = http.createServer((req, res) => {
     //El codigo respondera un codigo 200
     res.statusCode = 200;
     //El servidor respondera con un tenxto plano 
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
     //El mensaje respondera con el mensaje de hola munod
-    res.end('Hola Mundo\n');
+    res.end('<h1>Hola Mundo</h1>');
 });
 
 server.listen(port, hostname, () => {
