8f56188d35cd8140b7e66ea15d331e29fe7f31ed
BarayAlberto <bto450xd@gmail.com>
2022-04-28 11:30:43 -0600

    Adicion de minitipos para reconocer archivos

--- servidor.js
+++ servidor.js
@@ -9,4 +9,12 @@ http.createServer(function(request, respone){
     if(filePath == './'){
         filePath = "./index.html";
     }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var contentType = 'text/html';
+    var mimeTypes = {
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.png': 'image/png'
+    }
 });
