90f607e75b2fdf1d397326a14cc43a18a8b48ed8
BarayAlberto <bto450xd@gmail.com>
2022-04-28 11:47:29 -0600

    Adicion de mensajes de error en servidor.js

--- servidor.js
+++ servidor.js
@@ -17,4 +17,23 @@ http.createServer(function(request, respone){
         '.css': 'text/css',
         '.png': 'image/png'
     }
-});
+
+    fs.readFile(filePath, function(error, content){
+        if(error){
+            if(error.code = 'ENOENT'){
+                fs.readFile('./404.html', function(error, content){
+                    response.write.writeHead(200, {'Content-Type': contentType});
+                    response.end(content, 'utf-8');
+                });
+            }else{
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.responseEnd());
+            }
+        }else{
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(3000);
+console.log('Server running at http://127.16.0.39:3000/'); 
